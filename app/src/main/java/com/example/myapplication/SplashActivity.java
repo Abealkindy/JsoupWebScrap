3897268449fc0ed430d8f81a1b4f3a77ba7f144d
Muhammad Abraham Al Kindy <abealkindy@gmail.com>
2019-10-02 21:23:23 +0700

    [update] changed many things

--- app/src/main/java/com/example/myapplication/SplashActivity.java
+++ app/src/main/java/com/example/myapplication/SplashActivity.java
@@ -13,12 +13,9 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(SplashActivity.this, MainActivity.class));
-                finish();
-            }
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            finish();
         }, 3000);
     }
 
